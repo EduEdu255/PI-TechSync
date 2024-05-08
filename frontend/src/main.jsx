14782c9a1cba97bf4befb179ac88f4d7906333f5
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2024-05-08 11:31:04 -0300

    Ajuste Importação css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
 //Importar Rotas
 import Contact from './Routes/Contact.jsx';
