0671d4493b04bae7dd5baae696ab58d8f44dbbae
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2024-05-26 16:09:59 -0300

    Frontend - Construção Início menu

---
+++ frontend/src/Routes/Cia/MenuCia.jsx
@@ -0,0 +1,43 @@
+import { BsGraphUp, BsFillAirplaneFill, BsBagFill } from "react-icons/bs";
+import { HiOutlineLogout } from "react-icons/hi";
+
+import { Link, Outlet } from "react-router-dom";
+
+function SidebarMenuItem({ icon, text, to }) {
+  return (
+    <Link
+      to={to}
+      className="flex gap-5 items-center px-5 py-3 hover:bg-[#3758D0] hover:text-white font-[Poppins] rounded-2xl transition-all text-lg"
+    >
+      <span>{icon}</span>
+      <span>{text}</span>
+    </Link>
+  );
+}
+
+function MenuCia() {
+  const menuItems = [
+    { icon: <BsGraphUp />, text: "Relatórios", to: "/cia/dashboard" },
+    { icon: <BsFillAirplaneFill />, text: "Aeronaves", to: "/cia/aeronave" },
+    { icon: <BsBagFill />, text: "Vôos", to: "/cia/voo" },
+    { icon: <HiOutlineLogout />, text: "Sair", to: "/logout" },
+  ];
+
+  return (
+    <div className="flex gap-1">
+      <div className="flex flex-col gap-3 w-[15%] px-7 items-center">
+        <img src="/images/logo pousar.svg" className="h-[80px]"></img>
+        <div>
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.text} {...item} />
+          ))}
+        </div>
+      </div>
+      <div className="w-full">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default MenuCia;
--- frontend/src/Services/LoginContext.jsx
+++ frontend/src/Services/LoginContext.jsx
@@ -1,38 +1,43 @@
 import React, { createContext, useState } from "react";
 
 export const LoginContext = createContext({
-    isLoggedIn: false,
-    loggedUser: null,
-    setIsLoggedIn: () => { },
-    setLoggedUser: () => { }
+  isLoggedIn: false,
+  loggedUser: null,
+  setIsLoggedIn: () => {},
+  setLoggedUser: () => {},
 });
 
 export function LoginContextProvider({ children }) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // Initial login state
-    const [loggedUser, setLoggedUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Initial login state
+  const [loggedUser, setLoggedUser] = useState(null);
 
-    const handleLogin = (user) => { 
-        setIsLoggedIn(true); // Example login function
-        setLoggedUser(user);
+  const handleLogin = (user) => {
+    if (!user) {
+      return handleLogout();
     }
-    const handleLogout = () => {
-        setIsLoggedIn(false); // Example logout function
-        setLoggedUser(null);
-    }
+    setIsLoggedIn(true); // Example login function
+    sessionStorage.setItem("loggedUser", JSON.stringify(user));
+    setLoggedUser(user);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false); // Example logout function
+    setLoggedUser(null);
+    sessionStorage.setItem("loggedUser", null);
+  };
 
-    // You can add more functions to manage login state as needed
+  // You can add more functions to manage login state as needed
 
-    const contextValue = {
-      isLoggedIn,
-      loggedUser,
-      setIsLoggedIn: handleLogin, // Pass the actual function to update state
-      setLoggedUser: handleLogin,
-      handleLogout, // Can also provide additional helper functions
-    };
+  const contextValue = {
+    isLoggedIn,
+    loggedUser,
+    setIsLoggedIn: handleLogin, // Pass the actual function to update state
+    setLoggedUser: handleLogin,
+    handleLogout, // Can also provide additional helper functions
+  };
 
-    return (
-      <LoginContext.Provider value={contextValue}>
-        {children}
-      </LoginContext.Provider>
-    );
+  return (
+    <LoginContext.Provider value={contextValue}>
+      {children}
+    </LoginContext.Provider>
+  );
 }
