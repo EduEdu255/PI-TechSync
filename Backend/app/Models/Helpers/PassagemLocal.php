4fe8afefb23f889c8c827c4da2a0b306605da172
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2024-05-28 14:34:36 -0300

    Backend - Reogranização das classes de administração de vôos para trazer as viagens do banco de dados organizadas em ida e volta, separadas por cia aérea

--- Backend/app/Models/Helpers/PassagemLocal.php
+++ Backend/app/Models/Helpers/PassagemLocal.php
@@ -16,11 +16,9 @@ class PassagemLocal implements JsonSerializable
 
 
     /**
-     * @param Trecho[] $trechosIda
+     * @param Trecho[] $trechos
      *
      */
-
-    private string $link;
 
     public function __construct(
         private float $preco,
@@ -33,122 +31,18 @@ class PassagemLocal implements JsonSerializable
         private array $trechos,
         private Busca $busca,
     ) {
-        $this->setLink();
     }
-    public function getPreco(){
+    public function getPreco()
+    {
         return $this->preco ?? 0;
     }
 
-    private function setLink()
+    public function getCiaAerea()
     {
-        $cia = $this->ciaAerea;
-        $url = $cia->url;
-        $iata = $cia->codigo_iata;
-        $parts = parse_url($url);
-        parse_str($parts['query'], $params);
-        $link = match ($iata) {
-            'G3' => $this->getGolUrl($params),
-            'LA' => $this->getTamUrl($params),
-            'AD' => $this->getAzulUrl()
-        };
-        $this->link = $link;
+        return $this->ciaAerea;
     }
-
-    private function getGolUrl(array $params): string
-    {
-        //https://b2c.voegol.com.br/compra/busca-parceiros?pv=br&tipo=DF&de=JDO&para=EZE&ida=23-05-2024&volta=26-06-2024&ADT=1&ADL=0&CHD=0&INF=0
-        $base = 'https://b2c.voegol.com.br/compra/busca-parceiros?';
-        $params['de'] = $this->codOrigem;
-        $params['para'] = $this->codDestino;
-        $params['ida'] = $this->dataHoraSaida->format("d-m-Y");
-        if (!$this->busca->data_chegada) {
-            unset($params['volta']);
-        } else {
-            $params['volta'] = (new DateTime($this->busca->data_chegada))->format("d-m-Y");
-        }
-
-        return $base . $this->getQueryParamString($params);
-    }
-    private function getAzulUrl(): string
-    {
-        /**
-         *
-         * https://www.voeazul.com.br/br/pt/home/selecao-voo?
-         * c[0].ds=JDO&
-         * c[0].std=06/06/2024&
-         * c[0].as=GRU&
-         * c[1].ds=GRU&
-         * c[1].std=06/11/2024&
-         * c[1].as=JDO&
-         * p[0].t=ADT&
-         * p[0].c=1&
-         * p[0].cp=false&
-         * f.dl=3&
-         * f.dr=3&
-         * cc=BRL
-         */
-        $base = 'https://www.voeazul.com.br/br/pt/home/selecao-voo?';
-        $params = [];
-        $params['c'] = [];
-        $ida = ["ds" => $this->codOrigem, 'as' => $this->codDestino, 'std' => (new DateTime($this->busca->data_saida))->format("m/d/Y")];
-        $params['c'][] = $ida;
-        if ($this->busca->data_chegada) {
-            $volta = ['ds' => $this->codDestino, 'as' => $this->codOrigem, 'std' => (new DateTime($this->busca->data_chegada))->format("m/d/Y")];
-            $params['c'][] = $volta;
-        }
-        $params['cc'] = 'BRL';
-        $params['p'] = [['t' => 'ADT', 'cp' => 'false', 'c' => '1']];
-        $params['f.dl'] = '3';
-        $params['f.dr'] = '3';
-        $queryParams = [];
-        foreach ($params as $key => $value) {
-            if (is_array($value)) {
-                if (array_is_list($value)) {
-                    foreach ($value as $index => $valor) {
-                        foreach ($valor as $key2 => $value2) {
-                            $str = $key . "[" . $index . "]." . $key2 . "=" . $value2;
-                            $queryParams[] = $str;
-                        }
-                    }
-                } else {
-                    foreach ($value as $key2 => $value2) {
-                        $queryParams[] = $key2 . "=" . $value2;
-                    }
-                }
-            } else {
-                $queryParams[] = $key . "=" . $value;
-            }
-        }
-        return $base . implode("&", $queryParams);
-    }
-    private function getTamUrl(array $params): string
-    {
-        //https://www.latamairlines.com/br/pt/oferta-voos?origin=JDO&inbound=2024-06-21T12%3A00%3A00.000Z&outbound=2024-06-15T12%3A00%3A00.000Z&destination=FOR&adt=1&chd=0&inf=0&trip=RT&cabin=Economy&redemption=false&sort=RECOMMENDED
-        $base = 'https://www.latamairlines.com/br/pt/oferta-voos?';
-        $params['origin'] = $this->codOrigem;
-        $params['destination'] = $this->codDestino;
-        $params['outbound'] = $this->dataHoraSaida->format(DateTime::ATOM);
-        if (!$this->busca->data_chegada) {
-            $params['trip'] = 'OW';
-        } else {
-            try{
-                $params['inbound'] = (new DateTime($this->busca->data_chegada))->format(DateTime::ATOM);
-            } catch (\Throwable $th){
-                $params['inbound'] = 'null';
-            }
-
-        }
-
-        return $base . $this->getQueryParamString($params);
-    }
-
-    private function getQueryParamString(array $params)
-    {
-        $urlParams = [];
-        foreach ($params as $key => $value) {
-            $urlParams[] = "{$key}={$value}";
-        }
-        return implode("&", $urlParams);
+    public function getBusca(){
+        return $this->busca;
     }
 
     private static function calculateDuracao(string $saida, string $chegada)
@@ -173,7 +67,7 @@ class PassagemLocal implements JsonSerializable
     private static function durationAsInterval(int $duration): string
     {
         $minutos = $duration % 60;
-        $horas =  (int) ($duration /60);
+        $horas =  (int) ($duration / 60);
         $interval = "PT";
         if ($horas) {
             $interval .= "{$horas}H";
@@ -188,7 +82,7 @@ class PassagemLocal implements JsonSerializable
     {
         $passagensIda = [];
         $passagensVolta = [];
-        
+
         foreach ($idas as $ida) {
             $dataSaida = new DateTime($busca->data_saida);
             $dataChegada = new DateTime($busca->data_saida);
@@ -215,7 +109,7 @@ class PassagemLocal implements JsonSerializable
                 $dataSaida = new DateTime($busca->data_chegada);
                 $dataChegada = new DateTime($busca->data_chegada);
                 $trechosVolta =
-                array_map('self::mapVooLocalToTrecho', $volta);
+                    array_map('self::mapVooLocalToTrecho', $volta);
                 $valor = array_reduce($volta, fn ($carry, $tr) => $carry + $tr->valor, 0);
                 $duration = self::calculateDuracao($volta[0]->hora_saida, $volta[count($volta) - 1]->hora_chegada);
                 $duration = self::durationAsInterval($duration);
@@ -234,9 +128,21 @@ class PassagemLocal implements JsonSerializable
                 //$passagem = new Passagem($preco, $cia, $codOrigem, $codDestino, $dataHoraSaidaIda, $dataHoraChegadaIda, $duracaoIda, $trechosIda)
             }
         }
-        usort($passagensIda, fn(PassagemLocal $passagem1, PassagemLocal $passagem2) => $passagem1->getPreco() > $passagem2->getPreco());
-        usort($passagensVolta, fn(PassagemLocal $passagem1, PassagemLocal $passagem2) => $passagem1->getPreco() > $passagem2->getPreco());
+        usort($passagensIda, fn (PassagemLocal $passagem1, PassagemLocal $passagem2) => $passagem1->getPreco() > $passagem2->getPreco());
+        usort($passagensVolta, fn (PassagemLocal $passagem1, PassagemLocal $passagem2) => $passagem1->getPreco() > $passagem2->getPreco());
         return ["ida" => $passagensIda, "volta" => $passagensVolta];
+    }
+
+    public function juntaIdasEVoltas(array $idas, array $voltas = [])
+    {
+        if (count($voltas) == 0) {
+            return $idas;
+        }
+        $retorno = [];
+
+        $menor = count($idas) < count($voltas) ? count($idas) : count($voltas);
+        for ($i = 0; $i < $menor; $i++) {
+        }
     }
 
     public function jsonSerialize(): mixed
@@ -245,14 +151,13 @@ class PassagemLocal implements JsonSerializable
             [
                 "origem" => $this->codOrigem,
                 "destino" => $this->codDestino,
-                "ciaAerea" => $this->ciaAerea,
                 "preco" => number_format($this->preco, 2, ",", "."),
-                "linkBusca" => $this->link ?? '',
                 "dataHoraSaida" => $this->dataHoraSaida->format(DateTime::ATOM),
                 "dataHoraChegada" => $this->dataHoraChegada->format(DateTime::ATOM),
                 "duracao" => $this->duracao,
                 "trocaAeroporto" => false,
-                "trechos" => $this->trechos
+                "trechos" => $this->trechos,
+                "paradas" => count($this->trechos) - 1,
             ];
         return $retorno;
     }
