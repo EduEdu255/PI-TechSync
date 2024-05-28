fc43c32e9ac6b41c39934a6971026ede66084c8e
Eric José <ericjose.s2016@hotmail.com>
2024-05-28 14:20:34 -0300

    Card do plaoo anual - retirei metodo de pagamento, para fazer dessa pagina apenas um catalogo.

--- frontend/src/Routes/Planos.jsx
+++ frontend/src/Routes/Planos.jsx
@@ -4,18 +4,30 @@ const Planos = () => {
     return (
         <div className="bg-gradient-to-r from-purple-900 to-blue-700 text-white h-screen">
           <div className="container mx-auto py-10">
-            <h2 className="text-center text-3xl font-bold mb-6">Escolha sua assinatura para melhor empresa.</h2>
+            <h2 className="text-center text-3xl font-bold mb-6">Conheça nossos planos e escolha a melhor opção para sua empresa.</h2>
             <div className="flex justify-center space-x-4">
+             
+             {/* Plano Enterprise */}
+             <div className="bg-black p-6 rounded-lg shadow-md w-1/4">
+                <h3 className="font-semibold text-3xl mb-4 text-white">Anual</h3>
+                <p className='font-bold mb-4 text-4xl text-white' >R$ 749,99</p>
+                <p className='font-normal mb-4 text-xl text-white' >Nesse plano su empresa estará economizando, aderindo a essa assinatura sua mensalidade sairá no valor de...</p>
+                <p className='font-bold mb-4 text-4xl text-white' >R$ 62,50</p>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
+              </div>
+
               {/* Plano Básico */}
               <div className="bg-white p-6 rounded-lg shadow-md w-1/4">
-                <h3 className="font-semibold text-xl text-blackmb-4">Básico</h3>
+                <h3 className="font-semibold text-xl text-black text-blackmb-4">Mensal</h3>
                 {/* Detalhes do plano... */}
                 <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
               </div>
               
-               {/* Plano Enterprise */}
-               <div className="bg-black p-6 rounded-lg shadow-md w-1/4">
-                <h3 className="font-semibold text-xl mb-4 text-white">Enterprise</h3>
+
+              {/* Plano Pro */}
+              <div className="bg-white p-6 rounded-lg shadow-md w-1/4">
+                <h3 className="font-semibold text-xl text-black mb-4">Pro</h3>
+                <p className="font-light text-black mb-4">Este plano</p>
                 {/* Detalhes do plano... */}
                 <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
               </div>
@@ -30,20 +42,7 @@ const Planos = () => {
              
             </div>
 
-        {/* Formulário de Informações de Pagamento */}
-        <form action="#" method="POST" className="mt-10 bg-white text-gray-700 p-8 rounded-lg shadow-md max-w-md mx-auto">
-          <fieldset>
-            <legend className="text-center font-bold text-xl mb-6">Escolha a forma de pagamento</legend>
-
-            {/* Campo para número do cartão */}
-            <label htmlFor="card-number" className="block mb-2">Número do cartão:</label>
-            <input type="text" id="card-number" className="w-full p-2 border rounded-md" />
-
-            {/* Outros campos do formulário, caso queiram colocar, nos oriete com sua sabedoria mestre thormes kkkk */}
-
-            <button type="submit" className="mt-4 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Confirmar pagamento</button>
-          </fieldset>
-        </form>
+        
       </div>
 
       
