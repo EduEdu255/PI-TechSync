d34f8e81085714fa2b494c57490eae6cdaa956f9
Eric José <ericjose.s2016@hotmail.com>
2024-05-28 14:47:52 -0300

    botão para rediecionar e alinhamento da div

--- frontend/src/Routes/Planos.jsx
+++ frontend/src/Routes/Planos.jsx
@@ -13,41 +13,38 @@ const Planos = () => {
                 <p className='font-bold mb-4 text-4xl text-white' >R$ 749,99</p>
                 <p className='font-normal mb-4 text-xl text-white' >Nesse plano su empresa estará economizando, aderindo a essa assinatura sua mensalidade sairá no valor de...</p>
                 <p className='font-bold mb-4 text-4xl text-white' >R$ 62,50</p>
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
+                
               </div>
 
               {/* Plano Básico */}
               <div className="bg-white p-6 rounded-lg shadow-md w-1/4">
-                <h3 className="font-semibold text-xl text-black text-blackmb-4">Mensal</h3>
+                <h3 className="font-semibold text-3xl mb-4 text-black text-blackmb-4">Mensal</h3>
                 {/* Detalhes do plano... */}
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
+                
               </div>
               
 
               {/* Plano Pro */}
               <div className="bg-white p-6 rounded-lg shadow-md w-1/4">
-                <h3 className="font-semibold text-xl text-black mb-4">Pro</h3>
-                <p className="font-light text-black mb-4">Este plano</p>
+                <h3 className="font-semibold text-3xl mb-4 text-black mb-4">Pro</h3>
+                <p className="font-light text-black mb-4"></p>
                 {/* Detalhes do plano... */}
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
+
               </div>
 
               {/* Plano Pro */}
               <div className="bg-white p-6 rounded-lg shadow-md w-1/4">
-                <h3 className="font-semibold text-xl text-black mb-4">Pro</h3>
+                <h3 className="font-semibold text-3xl mb-4 text-black mb-4">Pro</h3>
                 {/* Detalhes do plano... */}
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Escolher esse plano</button>
               </div>
     
-             
             </div>
-
+            <div className='mt-10 p-8  max-w-md mx-auto flex items-center justify-center'>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 align-middle justify-center">Clique aqui para aderir seu plano...</button>
+            </div>
         
       </div>
-
-      
     </div>
-    
   );
 };
 
