35db5f8cc0017a5a6bcbff7a94c56cae526da923
EduEdu255 <joaoeduardoagsilvall2019@gmail.com>
2024-05-31 17:45:34 -0300

    Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { environment } from '../src/environment.js';
+import { environment } from '../environment.js';
 
 //Importar Rotas
 
